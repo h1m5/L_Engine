be6507eca99051e93125c0459bcfe110cb765fd1
Imhoisi Otokhagua <imhoisi@yahoo.com>
2015-03-25 04:42:51 +0300

    Initial Commit

---
+++ L_Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L_Engine
+//
+//  Created by Imhoisili Otokhagua on 25/03/15.
+//  Copyright (c) 2015 TriKomand. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
